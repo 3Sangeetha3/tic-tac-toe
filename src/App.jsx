6c2aed8ffa7f7e48d93fc5c54b9b00bc0366fabb
sangeetha <jadamalsangeetha@gmail.com>
2024-06-21 19:40:22 +0530

    update: css of board

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
+import { useState } from 'react';
 import './App.css'
 //React components are regular JavaScript functions, but their names must start with a capital letter or they won’t work!
 //There can be only one return statement in the component or else they must be wrapped in a enclosing tag i.e, a fragemnt or div. We cannot have adjacent jsx.
 
 //creating a component for squares in tic-tac-toe game
 
-function Square({value}) {
+function Square() {
+
+  const [value, setValue] = useState(null);
+
   function handleClick () {
-    alert(`${value} is clicked`);
+    setValue('X')
+    // alert(`${value} is clicked`)
   }
+
+  console.log(`square rendered`)
   return <button onClick={handleClick} className='square'>{value}</button>
 }
 
 
 function Board() {
+  console.log(`Board rendered`)
+
   return (
     <>
       <div className='board-row'>
-        <Square value="1"/>
-        <Square value={2}/>
-        <Square value={3}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value={4}/>
-        <Square value={5}/>
-        <Square value={6}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value={7}/> 
-        <Square value={8}/>
-        <Square value={9}/>
+        <Square /> 
+        <Square />
+        <Square />
       </div>
     </>
   )
@@ -37,7 +46,7 @@ function Board() {
 
 
 function App() {
-
+  console.log('App rendered')
   return (
       <div>
         <h1>Tic-Tac-Toe</h1>
