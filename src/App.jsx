5e1826c23d03ac1a952a3e3464c4773d8ee00782
sangeetha <jadamalsangeetha@gmail.com>
2024-07-01 15:55:36 +0530

    Update favicon and title in index.html, remove vite.svg file

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,114 @@ import './App.css'
 
 //creating a component for squares in tic-tac-toe game
 
-function Square() {
+function Square({value, onSquareClick}) {
+  // const [value, setValue] = useState(null);
+  //console.log('value', value)
 
-  const [value, setValue] = useState(null);
+  // function handleClick () {
+  //   setValue('X')
+  //   // alert(`${value} is clicked`)
+  // }
 
-  function handleClick () {
-    setValue('X')
-    // alert(`${value} is clicked`)
-  }
-
-  console.log(`square rendered`)
-  return <button onClick={handleClick} className='square'>{value}</button>
+  //console.log(`square rendered`)
+  return <button onClick={onSquareClick} className='square'>{value}</button>
 }
 
-
+//creating a component for board in tic-tac-toe game
 function Board() {
   console.log(`Board rendered`)
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  const [xIsNext, setXIsNext] = useState(true);
+
+  console.log('squares', squares)
+
+  //function to handle clicks of squares
+  function handleClick(i) {
+    console.log(i, "is clicked")
+
+    if(squares[i] || calculateWinner(squares)) return; //if the square already has an element then it will return inshort stops the value from being updated again.
+
+    //creating a copy of squares state 
+    const updatedSquares = squares.slice();
+
+    //flips to "X" and "O"
+    if(xIsNext){
+      updatedSquares[i] = 'X';
+    }
+    else{
+      updatedSquares[i] = 'O';
+    }
+  
+    setXIsNext(!xIsNext);
+    setSquares(updatedSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  console.log('winner', winner)
+
+  let status;
+  if(winner){
+    status = `🥳 Winner is : ${winner} 🥳`
+  }
+  //checking for the condition that all the squares are filled
+  else if(squares.every(square => square !== null)){
+    status = `😐 It's a draw 😐`
+  }
+  else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  }
 
   return (
     <>
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
-      <div className='board-row'>
-        <Square /> 
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   )
 }
 
+//calculating a winner
+function calculateWinner(squares){
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
 
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i=0; i<winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+
+    //checking for the same value in the squares
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 function App() {
-  console.log('App rendered')
+  //console.log('App rendered')
   return (
       <div>
         <h1>Tic-Tac-Toe</h1>
